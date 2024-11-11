1e09f5581d18933dfad385d59b9ea61ce41763f8
bhoirakshayk <107392986+bhoirakshayk@users.noreply.github.com>
2024-11-11 16:50:42 +0530

    Update scrollEffect.js

--- scrollEffect.js
+++ scrollEffect.js
@@ -1,33 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const fadeInElements = document.querySelectorAll(".fade-in");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll("section");
-    
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("visible");
+                observer.unobserve(entry.target);
             }
         });
     }, { threshold: 0.1 });
-    
-    sections.forEach(section => {
-        observer.observe(section);
-    });
+
+    fadeInElements.forEach(element => observer.observe(element));
 });
-
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const fadeInElements = document.querySelectorAll(".fade-in");
-
-//     const observer = new IntersectionObserver(entries => {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 entry.target.classList.add("visible");
-//                 observer.unobserve(entry.target);
-//             }
-//         });
-//     }, { threshold: 0.1 });
-
-//     fadeInElements.forEach(element => observer.observe(element));
-// });
